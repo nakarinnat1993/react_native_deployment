2e2c3de6cf409078b3e679dab495c2adef282a0b
Nakarin Jaiseengam <nakarinnat1993@gmail.com>
2025-06-10 17:20:39 +0700

    Add screen FlexboxLayout

---
+++ screens/FlexboxLayout.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <>
+        <View style={styles.container}>
+            <View style={[styles.box, { backgroundColor: 'tomato' }]} />
+            <View style={[styles.box, { backgroundColor: 'gold' }]} />
+            <View style={[styles.box, { backgroundColor: 'skyblue' }]} />
+        </View>
+        <View style={{ flexDirection: 'row'}}>
+            <View style={{ backgroundColor: 'red', flex: 1, height: 100 }} />
+            <View style={{ backgroundColor: 'green', flex: 2, height: 100 }} />
+            <View style={{ backgroundColor: 'blue', flex: 1, height: 100 }} />
+        </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  box: {
+    width: 100,
+    height: 100,
+  },
+});
+
+export default App;
